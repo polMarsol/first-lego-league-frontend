18bbc1de562f81de2a41b894727fceffb6b0d7c4
Jan Castells <jan.castells@outlook.com>
2026-03-02 18:52:48 +0100

    Build solucionat

--- src/api/recordApi.ts
+++ src/api/recordApi.ts
@@ -1,37 +1,37 @@
-import type { AuthProvider } from "@/lib/authProvider";
+import type { AuthStrategy } from "@/lib/authProvider";
 import { Record } from "@/types/record";
 import { User } from "@/types/user";
 import { getHal, mergeHal, mergeHalArray, postHal } from "./halClient";
 
 export class RecordService {
-    constructor(private readonly authProvider: AuthProvider) {
+    constructor(private readonly authStrategy: AuthStrategy) {
     }
 
     async getRecords(): Promise<Record[]> {
-        const resource = await getHal('/records', this.authProvider);
+        const resource = await getHal('/records', this.authStrategy);
         const embedded = resource.embeddedArray('records') || [];
         return mergeHalArray<Record>(embedded);
     }
 
     async getRecordById(id: string): Promise<Record> {
-        const resource = await getHal(`/records/${id}`, this.authProvider);
+        const resource = await getHal(`/records/${id}`, this.authStrategy);
         return mergeHal<Record>(resource);
     }
 
     async getRecordsByOwnedBy(owner: User): Promise<Record[]> {
         const resource = await getHal(
-            `/records/search/findByOwnedBy?user=${owner.uri}`, this.authProvider);
+            `/records/search/findByOwnedBy?user=${owner.uri}`, this.authStrategy);
         const embedded = resource.embeddedArray('records') || [];
         return mergeHalArray<Record>(embedded);
     }
 
     async createRecord(record: Record): Promise<Record> {
-        const resource = await postHal('/records', record, this.authProvider);
+        const resource = await postHal('/records', record, this.authStrategy);
         return mergeHal<Record>(resource);
     }
 
     async getRecordRelation<T>(record: Record, relation: string): Promise<T> {
-        const resource = await getHal(record.link(relation).href, this.authProvider);
+        const resource = await getHal(record.link(relation).href, this.authStrategy);
         return mergeHal<T>(resource);
     }
 }
--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,32 +1,32 @@
-import type { AuthProvider } from "@/lib/authProvider";
+import type { AuthStrategy } from "@/lib/authProvider";
 import { User } from "@/types/user";
 import { getHal, mergeHal, mergeHalArray, postHal } from "./halClient";
 
 export class UsersService {
-    constructor(private readonly authProvider: AuthProvider) {
+    constructor(private readonly authStrategy: AuthStrategy) {
     }
 
     async getUsers(): Promise<User[]> {
-        const resource = await getHal('/users', this.authProvider);
+        const resource = await getHal('/users', this.authStrategy);
         const embedded = resource.embeddedArray('users') || [];
         return mergeHalArray<User>(embedded);
     }
 
     async getUserById(id: string): Promise<User> {
-        const resource = await getHal(`/users/${id}`, this.authProvider);
+        const resource = await getHal(`/users/${id}`, this.authStrategy);
         return mergeHal<User>(resource);
     }
 
     async getCurrentUser(): Promise<User | null> {
-        if (!await this.authProvider.getAuth()) {
+        if (!await this.authStrategy.getAuth()) {
             return null;
         }
-        const resource = await getHal('/identity', this.authProvider);
+        const resource = await getHal('/identity', this.authStrategy);
         return mergeHal<User>(resource);
     }
 
     async createUser(user: User): Promise<User> {
-        const resource = await postHal('/users', user, this.authProvider);
+        const resource = await postHal('/users', user, this.authStrategy);
         return mergeHal<User>(resource);
     }
 }
